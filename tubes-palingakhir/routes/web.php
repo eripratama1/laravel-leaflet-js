6ef69653b0eba24e6a68bca357d384f5bcff6e3c
wiicaakkk <bagus.wicaksono285@gmail.com>
2022-04-12 14:58:22 +0700

    First Commit

---
+++ tubes-palingakhir/routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\PlaceMapController;
+use App\Http\Controllers\PlaceController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\DataController;
+
+use App\Models\Place;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Auth::routes();
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::get('/', [PlaceMapController::class, 'index'])->name('frontpage');
+Route::get('/place/data', [DataController::class, 'places'])->name('place.data'); // DATA TABLE CONTROLLER
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('places', 'App\Http\Controllers\PlaceController');
+});
